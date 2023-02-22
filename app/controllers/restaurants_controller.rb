04157f45cfd790b9f86a13701da480b239c2e567
Florian de Radiguès <florian.radigues@gmail.com>
2023-02-22 15:12:37 +0100

    restaurants set

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,3 +1,40 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: %i[show destroy]
 
+  def index
+    @restaurants = Restaurant.all
+    @restaurant = Restaurant.new
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    @restaurant.save
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurant_path(@restaurant), status: :see_other
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
 end
